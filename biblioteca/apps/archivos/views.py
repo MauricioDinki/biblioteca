c6a9882741d63535a9e8075dc31c2c850d50405b
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-27 10:08:56 -0500

    Nuevas consultas para validar los tipos de usuario y los permisos

--- biblioteca/apps/archivos/views.py
+++ biblioteca/apps/archivos/views.py
@@ -7,19 +7,23 @@ from django.views.generic import View
 from biblioteca.apps.core.mixins import LoginRequiredMixin
 from biblioteca.apps.categorias.models import Categoria
 
-from .models import Archivo
+from .models import Archivo, Link
 
 
 class ListaArchivos(LoginRequiredMixin, View):
     template_name = 'archivos/lista_archivos.html'
 
     def get(self, request):
-        archivos = Archivo.objects.all()
         categorias = Categoria.objects.all()
+        archivos = Archivo.objects.all()
+        links = Link.objects.all()
 
         context = {
             'archivos': archivos,
-            'categorias': categorias,
+            'links': links,
+            'categorias_embajador': categorias.filter(embajador=True),
+            'categorias_emma': categorias.filter(emma=True),
+            'categorias_intern': categorias.filter(intern=True),
         }
 
         return render(request, self.template_name, context)
