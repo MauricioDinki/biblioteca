04e2912851d7fe1f933ce8977e72e38f32cdc844
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-27 10:08:27 -0500

    Creado el modelo de link

--- biblioteca/apps/archivos/models.py
+++ biblioteca/apps/archivos/models.py
@@ -33,3 +33,28 @@ class Archivo(models.Model):
 
     def __unicode__(self):
         return self.nombre
+
+
+class Link(models.Model):
+    link = models.URLField(
+        blank=False,
+        null=False,
+    )
+    categoria = models.ForeignKey(
+        Categoria,
+        blank=False,
+        null=False,
+    )
+
+    titulo = models.CharField(
+        blank=False,
+        null=False,
+        max_length=30,
+    )
+
+    class Meta:
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
+
+    def __unicode__(self):
+        return self.titulo
